327bd3a7140d38d0713afb5d3f2cfe6d78e9587a
manendra.rawat <mannuatnri.itm@gmail.com>
2021-06-25 08:53:08 +0530

    added export for container to hook child app

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,6 +1,21 @@
 import faker from 'faker';
 
-const cartText = `<div>you have ${faker.random.number()} items in your cart</div>`;
 
+const mount = (el) => {
+    const cartText = `<div>you have ${faker.random.number()} items in your cart</div>`;
+    el.innerHTML = cartText;
+}
 
-document.querySelector('#cart-dev').innerHTML = cartText;
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#cart-dev');
+  
+    // Assuming our container doesnt have an element
+    // with id 'dev-products'....
+    if (el) {
+      // We are probably running in isolation
+      mount(el);
+    }
+  }
+
+export { mount };
+
